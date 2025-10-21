28d9b3bb1a27b9caa12cbd4372c13db2fc0acec8
deddyhidayatullah755-afk <deddyhidayatullah755@gmail.com>
2025-10-21 13:26:54 +0700

    Add greeting button functionality in scrip.js

---
+++ scrip.js
@@ -0,0 +1,7 @@
+// 1. Pilih tobol berdasarkan ID-nya
+conts tombolsapa=documen.getElementByld('sapaButton')
+// 2. Tambahkan 'event listener' untuk aksi 'clik
+tombolSapa.addEventListener('click',function(){
+ //3. Jalankan kode ini ketika tombol di-klik
+ alert('halo! Terima kasih sudah berkunjung!');
+});
